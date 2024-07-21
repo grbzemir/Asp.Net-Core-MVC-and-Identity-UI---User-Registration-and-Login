58a0bd20d6f1ac208f6da54f940c6b6fe0f88d21
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-07-21 18:56:44 +0300

    Editting

--- AuthSystem/Program.cs
+++ AuthSystem/Program.cs
@@ -11,6 +11,13 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireUppercase = false;
+});
+
 
 var app = builder.Build();
 
